64a82361ad4c404728ebd90d8ef67666c6bc4f28
Liza-Cherevko <cherevkoelp@gmail.com>
2022-07-29 17:10:30 +0300

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,47 @@
 // Custom scripts
 
-Мобильное меню бургер
-function burgerMenu() {
-    const burger = document.querySelector('.burger')
-    const menu = document.querySelector('.menu')
-    const body = document.querySelector('body')
-    const navBtns = document.querySelector('.nav__btns')
+// Мобильное меню бургер
+// function burgerMenu() {
+//     const burger = document.querySelector('.burger')
+//     const menu = document.querySelector('.menu')
+//     const body = document.querySelector('body')
+//     const navBtns = document.querySelector('.nav__btns')
    
-    burger.addEventListener('click', () => {
-      if (!menu.classList.contains('active')) {
-        menu.classList.add('active')
-        burger.classList.add('active-burger')
-        body.classList.add('locked')
-        navBtns.classList.add('active')
-      } else {
-        menu.classList.remove('active')
-        burger.classList.remove('active-burger')
-        body.classList.remove('locked')
-        navBtns.classList.remove('active')
-      }
-    })
-    // Вот тут мы ставим брейкпоинт навбара
-    window.addEventListener('resize', () => {
-      if (window.innerWidth > 479.98) {
-        menu.classList.remove('active')
-        burger.classList.remove('active-burger')
-        body.classList.remove('locked')
-        navBtns.classList.remove('active')
-      }
-    })
-  }
-  burgerMenu()
+//     burger.addEventListener('click', () => {
+//       if (!menu.classList.contains('active')) {
+//         menu.classList.add('active')
+//         burger.classList.add('active-burger')
+//         body.classList.add('locked')
+//         navBtns.classList.add('active')
+//       } else {
+//         menu.classList.remove('active')
+//         burger.classList.remove('active-burger')
+//         body.classList.remove('locked')
+//         navBtns.classList.remove('active')
+//       }
+//     })
+//     // Вот тут мы ставим брейкпоинт навбара
+//     window.addEventListener('resize', () => {
+//       if (window.innerWidth > 479.98) {
+//         menu.classList.remove('active')
+//         burger.classList.remove('active-burger')
+//         body.classList.remove('locked')
+//         navBtns.classList.remove('active')
+//       }
+//     })
+//   }
+//   burgerMenu()
   
+
+$('.burger').on('click', function () { 
+  $('.menu-side').toggleClass('menu-side_active')
+})
+
+
+
+
+
+
 
   
 var swiper = new Swiper('.mySwiper', {
@@ -93,3 +103,6 @@ var swiper = new Swiper('.mySwiper', {
         
 //         }
 // });
+function viewDiv(){
+  document.getElementById("menu-side").style.display = "block";
+};
